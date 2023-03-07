8a699ff10e59ec96dbddbdca5f8f00b6f039b2cb
mallumo <develop.mallumo@gmail.com>
2023-03-07 20:07:07 +0100

    release/1.8.0-1.0.9-1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,18 +5,21 @@ pluginManagement {
         mavenCentral()
         maven("https://mallumo.jfrog.io/artifactory/gradle-dev-local")
     }
+
+    infix fun PluginDependencySpec.versionX(key: String): PluginDependencySpec = version(extra[key] as String)
+
     plugins {
-        kotlin("multiplatform").version(extra["version.kotlin"] as String)
-        kotlin("jvm").version(extra["version.kotlin"] as String)
-        kotlin("android").version(extra["version.kotlin"] as String)
-        id("com.android.application").version(extra["version.agp"] as String)
-        id("com.android.library").version(extra["version.agp"] as String)
-        id("com.google.devtools.ksp").version(extra["version.ksp"] as String)
+        kotlin("multiplatform") versionX "version.kotlin"
+        kotlin("jvm") versionX "version.kotlin"
+        kotlin("android") versionX "version.kotlin"
+        id("com.android.application") versionX "version.agp"
+        id("com.android.library") versionX "version.agp"
+        id("com.google.devtools.ksp") versionX "version.ksp"
     }
 }
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
