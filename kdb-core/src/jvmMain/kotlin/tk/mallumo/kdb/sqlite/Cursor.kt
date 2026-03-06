2759d3242c6cf8a0ca224e113469fb9a5abce9f8
mallumo <65978677+mallumoSK@users.noreply.github.com>
2026-03-06 16:45:54 +0100

    capability query string as clob data

--- kdb-core/src/jvmMain/kotlin/tk/mallumo/kdb/sqlite/Cursor.kt
+++ kdb-core/src/jvmMain/kotlin/tk/mallumo/kdb/sqlite/Cursor.kt
@@ -56,6 +56,15 @@ actual open class Cursor(val query: ResultSet, val isSqlite: Boolean) : Closeabl
         }.onFailure { it.printStackTrace() }
     }
 
+    actual open fun stringClob(index: Int, callback: (String) -> Unit) {
+        runCatching {
+            query.getClob(index + 1)?.also {
+                callback.invoke(it.characterStream.use { it.readText() })
+                it.free()
+            }
+        }.onFailure { it.printStackTrace() }
+    }
+
     actual open fun int(index: Int, callback: (Int) -> Unit) {
         runCatching {
             query.getInt(index + 1).also {
